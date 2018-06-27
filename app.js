20f107002598d754489918964d9473d0ab0a5b1a
arobcode2 <andrew-c-rob@live.com>
2018-06-27 12:04:25 -0500

    implemented grocery list

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 // TODO
 var App = () => (
-  <div>Hello, world!</div>
+  <div>
+    Hello, world!
+    <GroceryList />
+  </div>
+);
+
+var GroceryList = () => (
+  <ul>
+    <li>Apples</li>
+    <li>Oranges</li>
+  </ul>  
 );
 
 ReactDOM.render(<App />, document.getElementById('app'));
+//ReactDOM.render(<, document.getElementById('app'));
